b697d3a67ac5222066376a84941acc94dee8f826
DarkEye <lesko.matej.pu@gmail.com>
2020-04-26 01:26:32 +0200

    additional setup for ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata'
+import 'vue-class-component/hooks'
 import Vue from 'vue'
+import vuetify from './plugins/vuetify'
+import i18n from './i18n'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import i18n from './i18n'
 
 Vue.config.productionTip = false
 
