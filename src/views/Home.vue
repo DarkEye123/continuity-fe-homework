aad8bc1104def41c77aa2186f76aaa3e973e963d
DarkEye <lesko.matej.pu@gmail.com>
2020-04-27 02:34:52 +0200

    search WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,53 @@
       :options.sync="options"
       :server-items-length="mediaCount"
       :loading="loading"
+      :search="search"
       class="elevation-1"
-    ></v-data-table>
+    >
+      <template v-slot:top>
+        <v-toolbar flat color="white">
+          <!-- <v-toolbar-title text>{{ $t('media') }}</v-toolbar-title> -->
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn text v-on="on">
+                {{ filterBy.text }}
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in filters"
+                :key="index"
+                @click="filterBy = item"
+              >
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on }">
+              <v-btn color="primary" dark class="mb-2" v-on="on"
+                >New Item</v-btn
+              >
+            </template>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="edit(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small>
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <div class="uppercase" color="primary">
+          {{ $t('no-data') }} (ノಠ益ಠ)ノ彡┻━┻
+        </div>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
@@ -21,11 +66,55 @@ export default class Home extends Vue {
 
   options = {}
 
-  headers: Record<string, any>[] = []
+  dialog = false
+
+  search = ''
+
+  _filter = ''
+
+  get filters() {
+    const filtered = [...this.headers]
+    filtered.pop() //actions
+    console.log('rendering')
+    return filtered
+  }
+
+  get filterBy() {
+    return this.$data._filter || this.filters[0]
+  }
+
+  set filterBy(val: any) {
+    this.$data._filter = val
+  }
+
+  /**
+   * !!This is error prone and not an ideal solution!!
+   * I tried to use https://github.com/kimamula/ts-transformer-keys
+   * but it was without success - I couldn't make it work via chainWebpack
+   * though it would include some 'magic sauce' regarding readability
+   */
+  headers: Record<string, any>[] = [
+    {
+      align: 'start',
+      value: 'title',
+    },
+    {
+      value: 'type',
+    },
+    {
+      value: 'kind',
+    },
+    {
+      value: 'numberOfDiscs',
+    },
+    {
+      value: 'releaseYear',
+    },
+    { value: 'actions', sortable: false },
+  ]
 
   @Watch('options', { deep: true })
   onOptionChanged(val: any) {
-    console.log(val)
     const { page, itemsPerPage: perPage, sortBy: _sortBy, sortDesc } = val
     const sortBy = _sortBy[0]
     const orderDesc = !!sortDesc[0]
@@ -34,34 +123,8 @@ export default class Home extends Vue {
 
   @Watch('$i18n.locale')
   onLocaleChange() {
-    /**
-     * !!This is error prone and not an ideal solution!!
-     * I tried to use https://github.com/kimamula/ts-transformer-keys
-     * but it was without success - I couldn't make it work via chainWebpack
-     */
-    this.headers = [
-      {
-        text: this.$t('title'),
-        align: 'start',
-        value: 'title',
-      },
-      {
-        text: this.$t('type'),
-        value: 'type',
-      },
-      {
-        text: this.$t('kind'),
-        value: 'kind',
-      },
-      {
-        text: this.$t('numberOfDiscs'),
-        value: 'numberOfDiscs',
-      },
-      {
-        text: this.$t('releaseYear'),
-        value: 'releaseYear',
-      },
-    ]
+    this.headers.map((e) => (e.text = this.$t(e.value)))
+    this.headers = [...this.headers]
   }
 
   get media() {
@@ -78,3 +141,9 @@ export default class Home extends Vue {
   }
 }
 </script>
+
+<style scoped>
+.uppercase {
+  text-transform: uppercase;
+}
+</style>
